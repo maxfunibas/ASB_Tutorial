f1e8bf6101cd521c599feacb25da5c3c204f6ad0
Massimiliano Filipozzi <massimiliano.filipozzi@unibas.ch>
2021-10-03 18:18:18 +0200

    Changed color of ABS script from black to red

--- matlab_scripts/script_4.m
+++ matlab_scripts/script_4.m
@@ -6,7 +6,7 @@
 %read in data
 data = csvread('data/ASB.csv');
 %plot data
-plot(data(:,1), data(:,2), 'k-', 'LineWidth',7);
+plot(data(:,1), data(:,2), 'r-', 'LineWidth',7); % Change color from black (k) to red (r)
 
 %% SOLUTION: Breakout 1
 
